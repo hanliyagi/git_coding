9561992a1097bbed68c626c840bef925129bd574
hanliyagi <hanliyagi1@gmail.com>
2023-12-05 23:23:27 +0900

    코드 수정

--- lab13.cpp
+++ lab13.cpp
@@ -1,24 +1,52 @@
-#pragma once
-#ifndef ALT_TEXT2_H_INCLUDED 
-#define ALT_TEXT2_H_INCLUDED
+#include <iostream>
+#include <vector>
 #include <string>
-// The kinds of text objects supported (added Bounded) 
-enum class TextType { Plain, Fancy, Fixed, Bounded };
-class AltText2 {
-	TextType type;
-	std::string text;
-	std::string left_bracket;
-	std::string right_bracket;
-	std::string connector;
-	size_t length_limit;// Maximum number of characters in the text 
+#include <fstream>
+using namespace std;
+
+//예외처리: try/catch구문, throw문
+//1. 예외가 발생하는 부분을 try에 넣는다
+//2. 예외가 발생하면 catch 구문이 실행된다
+class FileNotFoundException : public exception {
+	string message;
 public:
-	AltText2(TextType type, const std::string& t);
-	AltText2(TextType type, const std::string& t, const std::string& left,
-		const std::string& right, const std::string& conn);
-	AltText2(TextType type);
-	// Constructor for a length-bounded text object
-	AltText2(TextType type, const std::string& t, size_t limit);
-	std::string get() const;
-	void append(const std::string& extra);
+	FileNotFoundException(string _m) :
+		message("File not found: " + _m) {}
+	virtual const char* what() const throw() {
+		return message.c_str();
+	}
+
 };
-#endif
+vector<int> load_vector(string filename) {
+	ifstream fin(filename);
+
+	// 파일이 열리지 않으면(파일이 존재하지x)
+	if (!fin) {
+		// 예외처리 (throw)
+		throw FileNotFoundException(filename);
+	}
+
+	vector<int> result;
+	int num, value;
+	// 파일로부터 값을 result에 저장
+	// 파일의 form: size, elements (5 1 2 3 4 4)
+	fin >> num;
+	for (int i = 0; i < num; i++) {
+		fin >> value;
+		result.push_back(value);
+	}
+	return result;
+}
+int main() {
+	try {
+		vector<int> v = load_vector("values.dat"); // values.dat 파일에서 vector 로드
+		for (int elem : v)
+			cout << elem << ' ';
+		cout << endl;
+	}
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	return 0;
+}
+
